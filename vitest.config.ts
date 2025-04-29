cccf6e1a463e15a527a3c5564da28f054543daa0
서주희 <maywngml@naver.com>
2025-04-30 03:56:46 +0900

    chore: 테스트 환경에서도 절대경로 사용할 수 있도록 수정

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
   },
 })
