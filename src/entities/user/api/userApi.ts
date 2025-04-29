cd6c1ac6b3ca4471a23c13bc3cff74d1850c09c1
서주희 <maywngml@naver.com>
2025-04-30 02:49:13 +0900

    feat: user get api 호출 함수 구현 및 response 타입 정의

---
+++ src/entities/user/api/userApi.ts
@@ -0,0 +1,7 @@
+import { fetchApi } from "@/shared/api/fetchApi"
+import type { User, GetUserResponse } from "../model/type"
+
+export const getUser = async (id: User["id"]) => {
+  const data = await fetchApi<GetUserResponse>({ method: "GET", url: `/api/users/${id}` })
+  return data
+}
--- src/entities/user/model/type.ts
+++ src/entities/user/model/type.ts
@@ -3,3 +3,73 @@ export interface User {
   username: string
   fullName: string
 }
+
+export interface Coordinates {
+  lat: number
+  lng: number
+}
+
+export interface Address {
+  address: string
+  city: string
+  coordinates: Coordinates
+  country: string
+  postalCode: string
+  state: string
+  stateCode: string
+}
+
+export interface Bank {
+  cardExpire: string
+  cardNumber: string
+  cardType: string
+  currency: string
+  iban: string
+}
+
+export interface Company {
+  address: Address
+  department: string
+  name: string
+  title: string
+}
+
+export interface Hair {
+  color: string
+  type: string
+}
+
+export interface Crypto {
+  coin: string
+  wallet: string
+  network: string
+}
+export interface GetUserResponse {
+  id: number
+  address: Address
+  age: number
+  bank: Bank
+  birthDate: string
+  bloodGroup: string
+  company: Company
+  crypto: Crypto
+  ein: string
+  email: string
+  eyeColor: string
+  firstName: string
+  gender: string
+  hair: Hair
+  height: number
+  image: string
+  ip: string
+  lastName: string
+  macAddress: string
+  maidenName: string
+  phone: string
+  role: string
+  ssn: string
+  university: string
+  userAgent: string
+  username: string
+  weight: number
+}
