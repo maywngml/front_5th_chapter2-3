5aa95f591647a2d8a0041bf298878ab03c01383c
서주희 <maywngml@naver.com>
2025-05-01 05:49:18 +0900

    chore: Header, Footer widgets > layout > ui로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom"
-import Header from "./widgets/ui/Header.tsx"
-import Footer from "./widgets/ui/Footer.tsx"
+import { Header, Footer } from "./widgets/layout/ui"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
 
 const App = () => {
