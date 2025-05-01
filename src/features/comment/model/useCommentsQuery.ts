ab3cd46097ad3b09cb6fe04148b145178f9d19fc
서주희 <maywngml@naver.com>
2025-05-02 03:15:37 +0900

    feat: useGetComments 훅 추가, api 호출 함수들 이름 수정

--- src/features/comment/model/useCommentsQuery.ts
+++ src/features/comment/model/useCommentsQuery.ts
@@ -1,15 +1,20 @@
 import { useMutation, useQuery } from "@tanstack/react-query"
 
-import {
-  addComment as addCommentApi,
-  updateComment as updateCommentApi,
-  deleteComment as deleteCommentApi,
-} from "@/entities/comment/api/commentsApi"
+import { getComments, addComment, updateComment, deleteComment } from "@/entities/comment/api/commentsApi"
 import type { Comment } from "@/entities/comment/model/type"
+
+export const useGetComments = () => {
+  return useMutation({
+    mutationFn: getComments,
+    onError: (e) => {
+      console.error("댓글 가져오기 오류:", e)
+    },
+  })
+}
 
 export const useAddComment = () => {
   return useMutation({
-    mutationFn: addCommentApi,
+    mutationFn: addComment,
     onError: (e) => {
       console.error("댓글 추가 오류:", e)
     },
@@ -18,7 +23,7 @@ export const useAddComment = () => {
 
 export const useUpdateComment = () => {
   return useMutation({
-    mutationFn: ({ id, body }: { id: Comment["id"]; body: Partial<Comment> }) => updateCommentApi(id, body),
+    mutationFn: ({ id, body }: { id: Comment["id"]; body: Partial<Comment> }) => updateComment(id, body),
     onError: (e) => {
       console.error("댓글 업데이트 오류:", e)
     },
@@ -27,7 +32,7 @@ export const useUpdateComment = () => {
 
 export const useDeleteComment = () => {
   return useMutation({
-    mutationFn: deleteCommentApi,
+    mutationFn: deleteComment,
     onError: (e) => {
       console.error("댓글 삭제 오류:", e)
     },
