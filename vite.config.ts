dc3e8eb3664556f9155b02835319376342dfba4d
서주희 <maywngml@naver.com>
2025-05-02 03:54:08 +0900

    chore: 배포 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { resolve } from "path"
 
+const isCI = process.env.CI === "true"
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: isCI ? "" : "/front_5th_chapter2-3/",
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
