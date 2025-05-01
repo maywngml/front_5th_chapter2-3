8780c2f3f7cdf33ad150fb0677c35b3148d782d6
서주희 <maywngml@naver.com>
2025-05-02 00:47:27 +0900

    feat: usePostsStore에 로딩 상태와 액션 추가

--- src/entities/post/model/usePostsStore.ts
+++ src/entities/post/model/usePostsStore.ts
@@ -3,6 +3,7 @@ import type { Post } from "../model/type"
 
 interface State {
   posts: Post[]
+  isLoading: boolean
   total: number
 }
 
@@ -11,10 +12,12 @@ interface Action {
   setPosts: (posts: State["posts"]) => void
   updatePost: (newPost: Post) => void
   deletePost: (postId: number) => void
+  setIsLoading: (isLoading: boolean) => void
 }
 
 export const usePostsStore = create<State & Action>((set) => ({
   posts: [],
+  isLoading: false,
   total: 0,
   addPost: (post) => set((state) => ({ ...state, posts: [post, ...state.posts] })),
   setPosts: (posts) => set(() => ({ posts, total: posts.length })),
@@ -22,4 +25,5 @@ export const usePostsStore = create<State & Action>((set) => ({
     set((state) => ({ ...state, posts: state.posts.map((post) => (post.id === newPost.id ? newPost : post)) })),
   deletePost: (postId) =>
     set((state) => ({ posts: state.posts.filter((post) => post.id !== postId), total: state.total - 1 })),
+  setIsLoading: (isLoading) => set((state) => ({ ...state, isLoading })),
 }))
