48875a5f9b16172492ec3f86218c9e1fa8c34347
서주희 <maywngml@naver.com>
2025-05-02 01:20:50 +0900

    chore: TanStack Query 설치, Provider에 TanStack Query 프로바이더 추가

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,19 @@
 import { ReactNode } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { CommentDialogProvider } from "@/features/comment/model/CommentDialogContext"
 import { PostDialogProvider } from "@/features/post/model/PostDialogContext"
 import { UserDialogProvider } from "@/features/user/model/UserDialogContext"
 
 export const AppProviders = ({ children }: { children: ReactNode }) => {
+  const queryClient = new QueryClient()
+
   return (
-    <CommentDialogProvider>
-      <PostDialogProvider>
-        <UserDialogProvider>{children}</UserDialogProvider>
-      </PostDialogProvider>
-    </CommentDialogProvider>
+    <QueryClientProvider client={queryClient}>
+      <CommentDialogProvider>
+        <PostDialogProvider>
+          <UserDialogProvider>{children}</UserDialogProvider>
+        </PostDialogProvider>
+      </CommentDialogProvider>
+    </QueryClientProvider>
   )
 }
